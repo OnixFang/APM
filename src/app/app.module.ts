e1ba2a863399ed2fd65849b3c190fc531bdeb060
Ulises Taveras <twily.dev@outlook.com>
2018-11-22 19:06:10 -0400

    Proceeded up to module 10 - vid 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,19 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ProductListComponent,
+        ConvertToSpacesPipe,
+        StarComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
